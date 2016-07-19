2041b5167b39195ea8c04ef69abc6f64d4d0dd95
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-07-19 11:15:15 -0400

    Updated the benchmarks

--- README.md
+++ README.md
@@ -11,8 +11,11 @@ python scripts/matrix_storage.py test/data/test.gz
 This tiny example yields the following output:
 
 ```
-Time performing range queries: 7.07 seconds (per query): 0.07 seconds
-Size of index: 313656 bytes
+[peter@computer matrix_storage_benchmark] [master]$ python scripts/matrix_storage.py test/data/test.gz
+Time creating index: 0.04 seconds
+Time performing range queries: 1.17 seconds (per query): 0.01 seconds
+Time outputting the index: 0.26
+Size of index: 37396 bytes
 ```
 
 If we use a larger contact map (5K resolution contact map of chromosome X) the 
@@ -20,6 +23,9 @@ performance degrades significantly. But that's ok, because the implementation
 here is but a naïve toy example. Other solutions will hopefully do better.
 
 ```
-Time performing range queries: 2829.92 seconds (per query): 28.30 seconds
-Size of index: 78334902 bytes
+[peter@computer matrix_storage_benchmark] [master]$ python scripts/matrix_storage.py test/data/chr21_chr22.tsv.gz
+Time creating index: 39.26 seconds
+Time performing range queries: 396.82 seconds (per query): 3.97 seconds
+Time outputting the index: 186.62
+Size of index: 28141265 bytes
 ```
--- scripts/matrix_storage.py
+++ scripts/matrix_storage.py
@@ -1,9 +1,11 @@
+import gzip
 import numpy as np
 import os.path as op
 import random
 import sys
 import time
 import argparse
+import negspy.coordinates as nc
 
 def main():
     parser = argparse.ArgumentParser(description="""
@@ -26,36 +28,59 @@ def main():
     args = parser.parse_args()
 
     t1 = time.time()
-    array = np.loadtxt(args.matrix_tsv[0])
+    array = []
 
-    print "Time loading structure: {:.2f} seconds".format(time.time() - t1)
+    with gzip.open(args.matrix_tsv[0], 'r') as f:
+        for line in f:
+            # convert each contact to global genomic coordinates by adding the
+            # chromosome name to its offset from the start of the genome
+            parts = line.split()
+            coord1 = nc.chr_pos_to_genome_pos(parts[0], int(parts[1]), 'hg19')
+            coord2 = nc.chr_pos_to_genome_pos(parts[2], int(parts[3]), 'hg19')
+            count = float(parts[4])
+            array += [[coord1, coord2, count]]
+
+    array = np.array(array)
+    print "Time creating index: {:.2f} seconds".format(time.time() - t1)
+
+    # the bounds of the contact coordinates
     min_x = min(array[:,0])
     min_y = min(array[:,1])
 
     max_x = max(array[:,0])
     max_y = max(array[:,1])
 
-    resolution = args.resolution
-    square_size = args.square
+    resolution = args.resolution   # The resolution of the contact data 
+    square_size = args.square      # The size of the square we wish to select
 
     t2 = time.time()
 
     for i in range(args.iterations):
+        # get the upper left corner of the rectangle to be selected
         point1 = (random.randint(min_x, max_x) / resolution) * resolution
         point2 = (random.randint(min_y, max_y) / resolution) * resolution
         selected_points = []
         
         for j in xrange(len(array)):
+            # select a rectangle
             if (array[j][0] >= point1 and array[j][0] < point1 + resolution * square_size and
                 array[j][1] >= point2 and array[j][1] < point2 + resolution * square_size):
                 selected_points += [array[j]]
-        print "range1: [", point1, point1 + resolution * square_size,  "] range2: [", point2, point2 + resolution * square_size, "] selected:", len(selected_points)
+
+        #print "range1: [", point1, point1 + resolution * square_size,  "] range2: [", point2, point2 + resolution * square_size, "] selected:", len(selected_points)
 
     t3 = time.time()
     print "Time performing range queries: {:.2f} seconds (per query): {:.2f} seconds".format(t3 - t2, (t3 - t2) / args.iterations)
+
+    with gzip.open('/tmp/tmp.tsv', 'w') as f:
+        for a in array:
+            chr_pos1 = nc.genome_pos_to_chr_pos(a[0])
+            chr_pos2 = nc.genome_pos_to_chr_pos(a[1])
+
+            f.write("{}\t{}\t{}\t{}\t{:.1f}".format(chr_pos1[0], chr_pos1[1], chr_pos2[0], chr_pos2[1], a[2]))
+
+    print "Time outputting the index: {:.2f}".format(time.time() - t3)
     print "Size of index: {} bytes".format(op.getsize(args.matrix_tsv[0]))
-
-
 
 if __name__ == '__main__':
     main()
