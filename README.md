e0665ec8258c8d8af839a27562bdb767b5636e33
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-07-20 14:35:48 -0400

    More benchmarks and an updated README

--- README.md
+++ README.md
@@ -2,7 +2,16 @@
 
 Test how well each contact matrix storage format performs.
 
-Example:
+**Data Example:**
+
+```
+[peter@dbmipkedjievmbp matrix_storage_benchmark] [master]$ gzcat test/data/chrX_5KB_bins.tsv.gz | head -n 3
+12      12      294.0
+12      13      3.0
+13      13      6.0
+```
+
+**Usage Example:**
 
 ```
 python scripts/matrix_storage.py test/data/test.gz
@@ -11,8 +20,16 @@ python scripts/matrix_storage.py test/data/test.gz
 This tiny example yields the following output:
 
 ```
-Time performing range queries: 7.07 seconds (per query): 0.07 seconds
-Size of index: 313656 bytes
+[peter@dbmipkedjievmbp matrix_storage_benchmark] [master]$ python scripts/matrix_storage.py test/data/test.gz
+Time loading structure: 0.07 seconds
+max_x: 183.0
+max_y: 184.0
+Time performing range queries (256x256): 0.82 seconds (per query): 0.01 seconds
+Time performing range queries (2048 x 2048): 0.80 seconds (per query): 0.01 seconds
+Time slicing across first dimension: 0.32 seconds (per query): 0.00 seconds
+Time slicing across second dimension: 0.32 seconds (per query): 0.00 seconds
+Time slicing across the diagonal: 0.25 seconds (per query): 0.00 seconds
+Size of index: 28937 bytes
 ```
 
 If we use a larger contact map (5K resolution contact map of chromosome X) the 
@@ -20,6 +37,14 @@ performance degrades significantly. But that's ok, because the implementation
 here is but a naïve toy example. Other solutions will hopefully do better.
 
 ```
-Time performing range queries: 2829.92 seconds (per query): 28.30 seconds
-Size of index: 78334902 bytes
+[peter@dbmipkedjievmbp matrix_storage_benchmark] [master]$ python scripts/matrix_storage.py test/data/chrX_5KB_bins.tsv.gz -i 10
+Time loading structure: 323.72 seconds
+max_x: 31047.0
+max_y: 31051.0
+Time performing range queries (256x256): 145.52 seconds (per query): 14.55 seconds
+Time performing range queries (2048 x 2048): 136.39 seconds (per query): 13.64 seconds
+Time slicing across first dimension: 82.92 seconds (per query): 8.29 seconds
+Time slicing across second dimension: 82.74 seconds (per query): 8.27 seconds
+Time slicing across the diagonal: 64.85 seconds (per query): 6.49 seconds
+Size of index: 69279301 bytes
 ```
--- scripts/matrix_storage.py
+++ scripts/matrix_storage.py
@@ -12,8 +12,6 @@ def main():
 """)
 
     parser.add_argument('matrix_tsv', nargs=1)
-    parser.add_argument('-r', '--resolution', default=5000,
-                                        help="The resolution of the data", type=int)
     parser.add_argument('-s', '--square', default=256,
                                         help="The size of the square within which to return values", type=int)
     parser.add_argument('-i', '--iterations', default=100,
@@ -35,24 +33,79 @@ def main():
     max_x = max(array[:,0])
     max_y = max(array[:,1])
 
-    resolution = args.resolution
+    print "max_x:", max_x
+    print "max_y:", max_y
+
     square_size = args.square
 
     t2 = time.time()
 
     for i in range(args.iterations):
-        point1 = (random.randint(min_x, max_x) / resolution) * resolution
-        point2 = (random.randint(min_y, max_y) / resolution) * resolution
+        point1 = random.randint(min_x, max_x)
+        point2 = random.randint(min_y, max_y)
         selected_points = []
         
+        # rectangular queries
         for j in xrange(len(array)):
-            if (array[j][0] >= point1 and array[j][0] < point1 + resolution * square_size and
-                array[j][1] >= point2 and array[j][1] < point2 + resolution * square_size):
+            if (array[j][0] >= point1 and array[j][0] < point1 + square_size and
+                array[j][1] >= point2 and array[j][1] < point2 + square_size):
                 selected_points += [array[j]]
-        print "range1: [", point1, point1 + resolution * square_size,  "] range2: [", point2, point2 + resolution * square_size, "] selected:", len(selected_points)
+        #print "range1: [", point1, point1 + square_size,  "] range2: [", point2, point2 + square_size, "] selected:", len(selected_points)
+
+    t25 = time.time()
+    print "Time performing range queries (256x256): {:.2f} seconds (per query): {:.2f} seconds".format(t25 - t2, (t25 - t2) / args.iterations)
+
+    for i in range(args.iterations):
+        point1 = random.randint(min_x, max_x)
+        point2 = random.randint(min_y, max_y)
+        selected_points = []
+        
+        # rectangular queries
+        for j in xrange(len(array)):
+            if (array[j][0] >= point1 and array[j][0] < point1 + square_size * 8 and
+                array[j][1] >= point2 and array[j][1] < point2 + square_size * 8):
+                selected_points += [array[j]]
+        #print "range1: [", point1, point1 + square_size,  "] range2: [", point2, point2 + square_size, "] selected:", len(selected_points)
 
     t3 = time.time()
-    print "Time performing range queries: {:.2f} seconds (per query): {:.2f} seconds".format(t3 - t2, (t3 - t2) / args.iterations)
+    print "Time performing range queries (2048 x 2048): {:.2f} seconds (per query): {:.2f} seconds".format(t3 - t25, (t3 - t25) / args.iterations)
+
+    for i in range(args.iterations):
+        point1 = random.randint(min_x, max_x)
+        selected_points = []
+
+        # slice along the x dimension, so grab all points where the all x coordinates equal a certain value
+        for j in xrange(len(array)):
+            if array[j][0] == point1:
+                selected_points += [array[j]]
+
+    t4 = time.time()
+    print "Time slicing across first dimension: {:.2f} seconds (per query): {:.2f} seconds".format(t4 - t3, (t4 - t3) / args.iterations)
+
+    for i in range(args.iterations):
+        point1 = random.randint(min_x, max_x)
+        selected_points = []
+
+        # slice along the y dimension, so grab all points where the all y coordinates equal a certain value
+        for j in xrange(len(array)):
+            if array[j][1] == point1:
+                selected_points += [array[j]]
+
+    t5 = time.time()
+    print "Time slicing across second dimension: {:.2f} seconds (per query): {:.2f} seconds".format(t5 - t4, (t5 - t4) / args.iterations)
+
+    for i in range(args.iterations):
+        selected_points = [0]
+
+        # slice along the diagonal, where x and y coordinates are equal
+        for point in array:
+            if point[0] == point[1]:
+                selected_points += [point]
+
+
+    t6 = time.time()
+    print "Time slicing across the diagonal: {:.2f} seconds (per query): {:.2f} seconds".format(t6 - t5, (t6 - t5) / args.iterations)
+
     print "Size of index: {} bytes".format(op.getsize(args.matrix_tsv[0]))
 
 
